97115319c03029e526354401cedab437c30a3b57
Isaias Zamora <ch16030060@chapala.tecmm.edu.mnx>
2021-09-03 13:26:03 -0500

    modificacion package.json actualizado

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 
 const getGifs = async (category) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=6Kjt83Rdh6kfBz7q3OC3BnjT7CKGgLrh`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=6Kjt83Rdh6kfBz7q3OC3BnjT7CKGgLrh`;
     const result = await fetch(url);
     const { data } = await result.json();
 
